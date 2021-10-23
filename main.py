f0b3149287e8e8fa1e47db1cc276e43051c7e19b
Manish <manish@Manishs-MacBook-Pro.local>
2021-10-23 12:06:01 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+name = 'Manish'
+print('Hello %s' %name)
+
+x=1
+y=2
+if x != y:
+    print('Not equal')
+else:
+    print('Equal')
+surname = 'Manis'
+if name == surname:
+    print('Equal')
+else:
+    print('Not Equal')
+print("%s is equal to %s : %s" %(name, surname,  (name == surname)))
